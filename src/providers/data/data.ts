6f6b81d96cae02d0ea9a16a19d405b4f753d26ba
JA1D3V <jp.freitas202@gmail.com>
2018-07-06 20:40:47 -0300

    Update data.ts

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -16,13 +16,13 @@ export class DataProvider {
 
   requestWeather(data, callback){
     console.log(data.coords.latitude)
-    this.http.get('https://api.openweathermap.org/data/2.5/weather?lat='+data.coords.latitude+'&lon='+data.coords.longitude+'&units=metric&appid=8fbadca1e05c1885835bfb7b87310d0d').subscribe(x => {
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?lat='+data.coords.latitude+'&lon='+data.coords.longitude+'&units=metric&appid={{sua key do openWeather}}').subscribe(x => {
       callback(x)
     })
   }
 
   requestNextDays(data, callback){
-    this.http.get('https://api.openweathermap.org/data/2.5/forecast?lat='+data.coords.latitude+'&lon='+data.coords.longitude+'&units=metric&appid=8fbadca1e05c1885835bfb7b87310d0d').subscribe(x => {
+    this.http.get('https://api.openweathermap.org/data/2.5/forecast?lat='+data.coords.latitude+'&lon='+data.coords.longitude+'&units=metric&appid={{sua key do openWeather}}').subscribe(x => {
       callback(x)
     })
   }
